6999dfbd4ed3f71a2e20c01823f02f2b2cfb06f3
plusIs <plusls@qq.com>
2017-03-01 21:41:29 +0800

    fix bugs 扫描平台端口有惊喜哦

--- catch_uestc_course.py
+++ catch_uestc_course.py
@@ -3,12 +3,11 @@ import optparse
 import threading
 import time
 
-import requests
-
 import uestc_login
 
 
-def get_mid_text(text, left_text, right_text, start = 0):
+def get_mid_text(text, left_text, right_text, start=0):
+    ''' 获取中间文本 '''
     left = text.find(left_text, start)
     if left == -1:
         return None
@@ -18,24 +17,26 @@ def get_mid_text(text, left_text, right_text, start = 0):
         return None
     return (text[left:right], left)
 def get_open_url_data(data):
+    '''读取选课网页'''
     url = 'http://eams.uestc.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id='
     while data[0] < 1000:
         data[1].acquire()
         num = data[0]
         data[0] += 1
         data[1].release()
-        r = data[2].get(url + str(num))
-        if '学号' in r.text:
+        response = data[2].get(url + str(num))
+        if '学号' in response.text:
             data[1].acquire()
             data.append(num)
             data[1].release()
 
-def get_open_url(u, threading_max = 50):
-    data = [0,threading.Lock(),u]
+def get_open_url(news_ession, threading_max=50):
+    '''获取抢课端口'''
+    data = [0, threading.Lock(), news_ession]
     threads = []
     while data[0] < 1000:
         if len(threads) <= min(threading_max, 1000 - data[0]):
-            threads.append(threading.Thread(target=get_open_url_data, args = (data, )))
+            threads.append(threading.Thread(target=get_open_url_data, args=(data, )))
             threads[len(threads) - 1].start()
     for i in threads:
         i.join()
@@ -43,14 +44,15 @@ def get_open_url(u, threading_max = 50):
     ret.sort()
     return ret
 
-def catch_course(u, port, id, choose = True, sleep = 0):
+def catch_course(new_session, port, id, choose=True, sleep=0):
+    '''抢课'''
     count = 0
     while True:
         postdata = {'operator0': '%s:%s:0' % (str(id), str(choose).lower())}
-        r = u.get(url = 'http://eams.uestc.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id=917') #获取jesession
-        r = u.post('http://eams.uestc.edu.cn/eams/stdElectCourse!batchOperator.action?profileId=917', data = postdata)
-        (info, end) = get_mid_text(r.text, 'text-align:left;margin:auto;">', '</br>')
-        info = info.replace(' ','').replace('\n','').replace('\t','')
+        new_session.get(url = 'http://eams.uestc.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id=%d' % port) #获取jesession
+        response = new_session.post('http://eams.uestc.edu.cn/eams/stdElectCourse!batchOperator.action?profileId=%d' % port, data=postdata)
+        info, end = get_mid_text(response.text, 'text-align:left;margin:auto;">', '</br>')
+        info = info.replace(' ', '').replace('\n', '').replace('\t', '')
         count += 1
         print('正在进行第%d次尝试    ' % (count, ))
         print(info)
@@ -67,15 +69,15 @@ parser.add_option('-P', '--port',
                   help="抢课端口（任意一个，可以用-g获得）")
 #parser.add_option('-t', '--time',
 #                  help="每次抢课的延时 单位为秒")
-parser.add_option('-g', '--getport', action = 'store_true',
+parser.add_option('-g', '--getport', action='store_true',
                   help="获取抢课端口")
 parser.add_option('-l', '--list',
                   help="课程编号 即?lesson.id=276731后的数字 格式：c1,c2,c3...")
 (options, args) = parser.parse_args()
 print(options)
-if options.num == None:
+if options.num is None:
     options.num = input('请输入你的学号:')
-if options.password == None:
+if options.password is None:
     options.password = getpass.getpass('请输入你的密码:')
 while True:
     if options.list != None:
@@ -91,7 +93,7 @@ while True:
     print('接下来输入课程编号')
     print('课程编号 即?lesson.id=276731后的数字 格式：c1,c2,c3...')
     options.list = input('请输入你的课程编号：')
-    
+
 print(options.list)
 while True:
     try:
@@ -101,20 +103,19 @@ while True:
         continue
     break
 
-u = uestc_login.login(options.num, options.password)
-if u == None:
-    print(uestc_login.uestcget_last_error())
+new_session = uestc_login.login(options.num, options.password)
+if new_session is None:
+    print(uestc_login.get_last_error())
     exit()
 print('登陆成功')
-print('开始抢课')
 if options.getport:
     print('url:\nhttp://eams.uestc.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id=')
-    print('port:\n' + str(get_open_url(u, threading_max = 50)))
+    print('port:\n' + str(get_open_url(new_session, threading_max=50)))
     exit()
-
+print('开始抢课')
 threads = []
 for i in options.list:
-    threads.append(threading.Thread(target=catch_course, args = (u, options.port, i, True, 0.5)))
+    threads.append(threading.Thread(target=catch_course, args=(new_session, options.port, i, True, 0.5)))
     threads[len(threads) - 1].start()
 #catch_course(u, options.port, 276926, False)
 for i in threads:
--- uestc_login.py
+++ uestc_login.py
@@ -1,48 +1,55 @@
+'''电子科技大学登陆'''
 import requests
-
-global error_text, error
-error_text = ['你的网炸了','密码错误']
-error = 0
-
-
-def get_mid_text(text, left_text, right_text, start = 0): #  获取中间文本
+__error__ = 0
+__all__ = ['login', 'get_last_error']
+def get_mid_text(text, left_text, right_text, start=0):
+    '''获取中间文本'''
     left = text.find(left_text, start)
     if left == -1:
-        return None
+        return ('', -1)
     left += len(left_text)
     right = text.find(right_text, left)
     if right == -1:
-        return None
+        return ('', -1)
     return (text[left:right], right)
 
 
 def login(num, password):
+    '''登陆'''
+    global __error__
     url = 'http://idas.uestc.edu.cn/authserver/login?service=http://portal.uestc.edu.cn/index.portal'
     # 获取lt,execution
-    u = requests.session()
-    u.cookies.clear()
+    new_session = requests.session()
+    new_session.cookies.clear()
     # r=u.get(url)
     try:
-        r = u.get(url)
+        response = new_session.get(url)
     except requests.exceptions.ConnectionError:
+        __error__ = 0
         return None
-    lt, end = get_mid_text(r.text, '"lt" value="', '"')
-    execution, end = get_mid_text(r.text, '"execution" value="', '"', end)
+    lt_data, end = get_mid_text(response.text, '"lt" value="', '"')
+    if end == -1:
+        __error__ = 0
+        return None
+    execution, end = get_mid_text(response.text, '"execution" value="', '"', end)
     # 构造表格
     postdata = {
         'username':num,
         'password':password,
-        'lt':lt,
+        'lt':lt_data,
         'dllt':'userNamePasswordLogin',
         'execution':execution,
         '_eventId':'submit',
         'rmShown':'1'
         }
-    r = u.post(url, data = postdata)
-    if '验证码' in r.text:
+    response = new_session.post(url, data=postdata)
+    if '验证码' in response.text:
+        __error__ = 1
         return None
-    return u
+    return new_session
 
 
 def get_last_error():
-    return error_text[error]
+    '''获取最后一次登录失败的错误'''
+    error_text = ['你的网炸了', '密码错误']
+    return error_text[__error__]
