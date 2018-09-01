f138cf59850f92b062303b9e3a5d1e501d5f950b
plusls <plusls@qq.com>
2018-09-02 02:06:10 +0800

    pip更新

--- uestc/__init__.py
+++ uestc/__init__.py
@@ -5,7 +5,7 @@ from .login import login
 from . import catch_course
 from . import query
 
-__version__ = "1.1.2.6"
+__version__ = "1.1.2.7"
 __author__ = "plusls<plusls@qq.com>"
 __name__ = "uestc"
 __doc__ = "一个和电子科大有关的模块"
