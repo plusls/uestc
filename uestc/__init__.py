b0cf37b441a29da0b71c87c9326e514fcd9710fd
plusls <plusls@qq.com>
2018-09-10 10:56:43 +0800

    修复import error

--- uestc/__init__.py
+++ uestc/__init__.py
@@ -1,11 +1,12 @@
 """一个和电子科大有关的模块"""
+import __main__
+if __main__.__doc__ != 'setup.py':
+    from . import exceptions
+    from .login import login
+    from . import catch_course
+    from . import query
 
-from . import exceptions
-from .login import login
-from . import catch_course
-from . import query
-
-__version__ = "1.1.2.7"
+__version__ = "1.1.2.8"
 __author__ = "plusls<plusls@qq.com>"
 __name__ = "uestc"
 __doc__ = "一个和电子科大有关的模块"
