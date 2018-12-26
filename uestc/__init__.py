2a3b0b8aa2d34be7e18398719a57b860da54393a
plusls <plusls@qq.com>
2018-12-26 19:26:05 +0800

    update version

--- uestc/__init__.py
+++ uestc/__init__.py
@@ -6,7 +6,7 @@ if __main__.__doc__ != 'setup.py':
     from . import catch_course
     from . import query
 
-__version__ = "1.1.2.8"
+__version__ = "1.1.2.9"
 __author__ = "plusls<plusls@qq.com>"
 __name__ = "uestc"
 __doc__ = "一个和电子科大有关的模块"
