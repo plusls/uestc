1edcc8f8b2aaf64ed7c6e13e576f5463b2c37b13
plusls <plusls@qq.com>
2019-06-03 22:35:06 +0800

    update white list

--- sample/query_score.py
+++ sample/query_score.py
@@ -4,18 +4,20 @@ import uestc
 import argparse
 import json
 
+
 def check_args(parser, args):
     if args.username is None:
         parser.error('请输入学号')
     if args.password is None:
         parser.error('请输入密码')
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--username',
-                      help='用户名')
+                        help='用户名')
     parser.add_argument('--password',
-                      help='密码')
+                        help='密码')
 
     args = parser.parse_args()
     check_args(parser, args)
@@ -30,24 +32,46 @@ def main():
     name_black_list = []
     name_black_list += ['实习']
 
+    type_white_list = ['思想政治', '英语', '学科通识', '学科基础', '学科拓展']
+    name_white_list = ['军事理论', '程序设计（C与C++）', '计算机网络', '数据库原理及应用', '软件工程及应用',
+                       '计算机系统结构', '汇编语言与微机接口技术',
+                       '综合素质实践Ⅰ', '综合素质实践Ⅱ', '数字逻辑综合实验', '综合课程设计',
+                       '计算机组成原理综合实验', '计算机系统结构综合实验', 
+                       '汇编语言与微机接口技术综合实验', '系统级软件综合课程设计', 
+                       '软件开发综合实验']
+
     score = 0
     credit = 0
     for semester in semesterid_data:
         course_list += uestc.query.get_score(login_session, semester)
     #course_list.append(uestc.query.Course('', '', '', 'test', '', '16', '', '', '90', ''))
 
-    for course in course_list:
-        next_course = False
-        for type_name in type_black_list:
-            if type_name in course.type:
-                next_course = True
-                break
+    is_black = False
 
-        for name in name_black_list:
-            print(name)
-            if name in course.name:
-                next_course = True
-                break
+    for course in course_list:
+        if is_black:
+            next_course = False
+            for type_name in type_black_list:
+                if type_name in course.type:
+                    next_course = True
+                    break
+            for name in name_black_list:
+                print(name)
+                if name in course.name:
+                    next_course = True
+                    break
+        else:
+            next_course = True
+            for type_name in type_white_list:
+                if type_name in course.type:
+                    next_course = False
+                    break
+            for name in name_white_list:
+                print(name)
+                if name in course.name:
+                    next_course = False
+                    break
+
         if next_course is False:
             try:
                 score += int(course.score)*float(course.credit)
